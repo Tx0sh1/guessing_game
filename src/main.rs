f1d061e1117285e8a9e8426c6ad5d2073ebb2dd2
Tx0sh1 <clydemojura@protonmail.com>
2025-05-01 21:45:23 +0200

    Implement number guessing game with input handling and lives counter

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,43 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("you guessed {}", guess);
-
     let secret_number = rand::random_range(1..101);
+    let mut lives = 5;
 
-    if guess < secret_number {
-        println!("Too small!");
-    } else if guess > secret_number {
-        println!("Too big!");
-    } else {
-        println!("You win!");
-    }
-    println!("The secret number was: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("you guessed {}", guess);
+
+        if guess < secret_number {
+            println!("Too small!");
+            lives = lives - 1;
+            println!("you have {} lives remaining", lives);
+                if lives == 0{
+                    println!("you are out of lives, game over");
+                    break;
+                }
+        } else if guess > secret_number {
+            println!("Too big!");
+            lives = lives - 1;
+            println!("you have {} lives remaining", lives);
+                if lives == 0{
+                    println!("you are out of lives, game over");
+                    break;
+                }
+        } else {
+            println!("You win!");
+            break;
+        }
+        //println!("The secret number was: {}", secret_number);
+        }
+    
 }
