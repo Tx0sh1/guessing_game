94a007490f02dd49ee54dcfcef97c14ecc3c6a94
Tx0sh1 <clydemojura@protonmail.com>
2025-05-02 20:52:30 +0200

    add comments

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::random_range(1..101);
-    let mut lives = 5;
+    let mut lives = 5; // ask user how many lkives they want
+    //add vector to store highscore for now
 
     loop {
         println!("Please input your guess.");
@@ -15,16 +16,17 @@ fn main() {
             .read_line(&mut guess)
             .expect("failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!"); //todo - handle invalid input without panicking
 
         println!("you guessed {}", guess);
 
+        //todo - change "if else" logic to "match"
         if guess < secret_number {
             println!("Too small!");
             lives = lives - 1;
             println!("you have {} lives remaining", lives);
                 if lives == 0{
-                    println!("you are out of lives, game over");
+                    println!("you are out of lives, game over!!");
                     break;
                 }
         } else if guess > secret_number {
@@ -32,7 +34,7 @@ fn main() {
             lives = lives - 1;
             println!("you have {} lives remaining", lives);
                 if lives == 0{
-                    println!("you are out of lives, game over");
+                    println!("you are out of lives, game over!!");
                     break;
                 }
         } else {
